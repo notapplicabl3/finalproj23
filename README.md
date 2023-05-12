c983860def566f4b12b760442242d18fd80cedf1
notapplicabl3 <132276940+notapplicabl3@users.noreply.github.com>
2023-05-12 15:53:30 +0000

    finishing up

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 final project for AP Computer Science A 2023.
 
 Goal: Determining the time efficiency of a student through various variables and returning it as a "score" + providing specific statistics for each "category" inputed
-  - not an extremely creative program, but just of personal interest
+  - for personal interest reasons
   - 
   
--- testMain.java
+++ testMain.java
@@ -7,14 +7,15 @@ public class testMain
 
         Scanner scan = new Scanner(System.in);
         
-        System.out.println("Welcome to the the Work Efficiency Tracker (Weff) ");
+        System.out.println("Welcome to the  ");
         System.out.println("There will be various questions asked to determine your work efficiency.");
         System.out.println("Our system will return a score corresponding to the levels of efficiency:");
         System.out.println("    0-200 - Very Poor");
         System.out.println("    200-400 - Poor");
         System.out.println("    400-600 - Ok");
-        System.out.println("    600-800 - Great");
-        System.out.println("    800-1000 - Excellent");
+        System.out.println("    600-800 - Good");
+        System.out.println("    800-1000 - Very Good");
+        System.out.println("    1000+ - Excellent");
         System.out.println("");
         System.out.println("");
         System.out.println("");
@@ -26,7 +27,7 @@ public class testMain
         boolean keepGoing = true;
 
         while (keepGoing){
-            System.out.println("Please put in the following values in hours unless otherwise specified");
+            System.out.println("Please put in the following values in hours unless otherwise specified (only input numbers)");
             System.out.println("How long was your work session? (include short breaks and not focused time, do not include time spent on extended breaks (> 15 minutes) (resting, eating, relaxing), these extended times of rest denote the time of break between work sessions)");
             double working = scan.nextDouble();
 
@@ -79,7 +80,7 @@ public class testMain
             
             System.out.println("Would you like to keep putting in more work sessions for today? (y/n)");
 
-            String yesno = scan.next();
+            String yesno = scan.next(); // using scan.next instead of nextLine because nextLine is not properly working consistently 
 
             if (yesno.equals("n")){
                 keepGoing = false;
@@ -91,6 +92,8 @@ public class testMain
 
 
         }
+
+        // printing out all the data
         System.out.println("");
         System.out.println("");
         System.out.println("Here are all your sessions and their stats + score:");
@@ -121,6 +124,75 @@ public class testMain
             System.out.println("");
         }
 
+        System.out.println("");
+        System.out.println("");
+        
+
+        System.out.println("Would you like to do anymore actions? (use credits, look at specific entry) (y/n)");
+        String moreActions = scan.next();
+        while (moreActions.equals("y")){
+            System.out.println("Please type \"c\" if you want to use credits, \"s\" if you want to look at a specific entry, \"q\" if you want to quit");
+            String action = scan.next();
+            if (action.equals("c")){ // action to use credits
+                System.out.println("Type in the session number you would like use credits from");
+                int sessionNum = scan.nextInt() - 1;
+                int creditsRemaining;
+                if (sessionNum >= arrayOfStats.size() || sessionNum < 0){
+                    System.out.println("");
+                    System.out.println("error");
+                } else {
+                    System.out.println("Using a credit...");
+                    System.out.println("");
+                    creditsRemaining = arrayOfCredits.get(sessionNum).useCredit();
+                    if (arrayOfCredits.get(sessionNum).getType().equals("better")){
+                        System.out.println("You used a better credit, have some fun. You earned it!");
+                    }
+                    if (arrayOfCredits.get(sessionNum).getType().equals("mid")){
+                        System.out.println("You used a mid credit, have some fun, but don't get carried away!");
+                    }
+                    if (arrayOfCredits.get(sessionNum).getType().equals("worse")){
+                        System.out.println("You used a worse credit, have some fun, and make sure you have a good work session next time!");
+                    }
+
+                    System.out.println("You have " + creditsRemaining + " creditsRemaining");
+                    System.out.println("");
+                }
+
+
+            } else if (action.equals("s")){ // action to look at specific work session
+                System.out.println("Type in the session number you would to look at");
+                int sessionNum = scan.nextInt() - 1;
+                if (sessionNum >= arrayOfStats.size() || sessionNum < 0){
+                    System.out.println("");
+                    System.out.println("error");
+                } else {
+                    System.out.println("Here you go");
+                    System.out.println("");
+                    System.out.println("\t Session " + (sessionNum + 1) + ": \n" + arrayOfStats.get(sessionNum));
+                    System.out.println("");
+                    System.out.println("SCORE: " + arrayOfStats.get(sessionNum).workScore());
+                    System.out.println("");
+                    System.out.println("");
+                    
+                }
+            } else if (action.equals("q")){
+                break;
+            } else {
+                System.out.println("error");
+            }
+
+            System.out.println("");
+            System.out.println("");
+            System.out.println("Would you like to do anymore actions? (use credits, look at specific entry) (y/n)");
+            moreActions = scan.next();
+
+
+        }
+
+
+        System.out.println("");
+        
+        System.out.println("See you next time!");
 
 
 
